41d22d4bfa6b4cb6cf409a39d3e17bd0f731e693
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-09-11 10:29:28 +0800

    rename landing page to home page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,4 @@
-class LandingController < ApplicationController
+class HomepageController < ApplicationController
   before_action :authenticate_employee!
   def index
     @employees = Employee.where(role: 'Employee')
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :employees
-  root 'landing#index'
+  root 'homepage#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
