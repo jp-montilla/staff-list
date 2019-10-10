5cabe0a9d2b1d1142950dd09f7f8a36f01e58629
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-10-10 11:32:54 +0800

    Fixed migration file

--- db/migrate/20190910080924_rename_show_in_list_in_questions.rb
+++ db/migrate/20190910080924_rename_show_in_list_in_questions.rb
@@ -1,5 +1,6 @@
 class RenameShowInListInQuestions < ActiveRecord::Migration[5.1]
   def change
+    add_column :questions, :view_in_list, :integer
     rename_column :questions, :view_in_list, :view_to_list
   end
 end
