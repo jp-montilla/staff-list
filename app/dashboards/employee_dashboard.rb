b33bdf145341d7dd1145e4feb8cb1b9bd492c46c
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-10-11 14:30:34 +0800

    Modified Admin employee show page

--- app/dashboards/employee_dashboard.rb
+++ app/dashboards/employee_dashboard.rb
@@ -38,12 +38,12 @@ class EmployeeDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :id,
+    # :id,
     :profile_picture,
     :name,
     :email,
     :role,
-    :created_at,
+    # :created_at,
     :material
   ].freeze
 
