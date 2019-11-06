1d2fed323d9ef505d0c0f2143e7d418db31c958d
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-11-06 15:12:12 +0800

    rename js file. rubocop until homepages controller

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
+
+# rubocop:disable Metrics/LineLength
+
+# Landing Page controller
 class HomepagesController < ApplicationController
   before_action :authenticate_employee!
+  before_action :fetch_index_data, only: [:index]
+  before_action :fetch_create_data, only: [:create]
+  before_action :fetch_update_data, only: [:update]
+  before_action :fetch_password_data, only: [:update_password]
   def index
-    @q = Employee.ransack(params[:q])
-    @employees = @q.result.order(name: :asc).page(params[:page]).per(15)
-    @questions = Question.where(view_to_list: 1)
     @answers = Answer.all
     respond_to do |format|
       format.js { render 'index.js.erb' }
@@ -33,26 +39,12 @@ class HomepagesController < ApplicationController
   end
 
   def create
-    @answer = Answer.new(set_params)
     if @answer.save
-      @employee = Employee.find(current_employee.id)
-      @answers = Answer.where(employee_id: current_employee.id)
-      @questions = Question.all
-      @answered = @answers.count
-      @unanswered = @questions.count - @answered
-      flash.now[:success] = 'Answer Saved'
-      respond_to do |format|
-        format.js { render 'fresh.js.erb' }
-        format.html
-      end
+      save_answer
     else
       flash.now[:errors] = @answer.errors.full_messages
-      @employee = Employee.find(current_employee.id)
       @question = @answer.question
-      respond_to do |format|
-        format.js { render 'add.js.erb' }
-        format.html
-      end
+      add
     end
   end
 
@@ -67,39 +59,22 @@ class HomepagesController < ApplicationController
   end
 
   def update
-    @answer = Answer.find(params[:id])
     if @answer.update(update_params)
       flash.now[:success] = 'Answer updated successfully.'
-      @employee = Employee.find(current_employee.id)
       @questions = Question.all
-      respond_to do |format|
-        format.js { render 'fresh.js.erb' }
-        format.html
-      end
+      refresh
     else
       flash.now[:errors] = @answer.errors.full_messages
-      @employee = Employee.find(current_employee.id)
       @answer_edit = @answer
-      respond_to do |format|
-        format.js { render 'edit.js.erb' }
-        format.html
-      end
+      render_edit
     end
   end
 
   def destroy
     @answer = Answer.find(params[:id])
     @answer.destroy
-    flash.now[:success] = 'Answer deleted successfully.'
-    @employee = Employee.find(current_employee.id)
-    @answers = Answer.where(employee_id: current_employee.id)
-    @questions = Question.all
-    @answered = @answers.count
-    @unanswered = @questions.count - @answered
-    respond_to do |format|
-      format.js { render 'fresh.js.erb' }
-      format.html
-    end
+    answer_destroy
+    refresh
   end
 
   def answer
@@ -130,17 +105,13 @@ class HomepagesController < ApplicationController
   end
 
   def update_password
-    @employee_detail = current_employee
     if @employee_detail.update_with_password(user_params)
       bypass_sign_in(@employee_detail)
       flash[:success] = 'Password changed!'
       render js: "window.location='#{homepage_path(current_employee.id)}'"
     else
       flash.now[:errors] = @employee_detail.errors.full_messages
-      respond_to do |format|
-        format.js { render 'random.js.erb' }
-        format.html
-      end
+      render_password
     end
   end
 
@@ -158,4 +129,74 @@ class HomepagesController < ApplicationController
     # NOTE: Using `strong_parameters` gem
     params.require(:employee).permit(:current_password, :password, :password_confirmation)
   end
+
+  def fetch_index_data
+    @q = Employee.ransack(params[:q])
+    @employees = @q.result.order(name: :asc).page(params[:page]).per(15)
+    @questions = Question.where(view_to_list: 1)
+  end
+
+  def fetch_create_data
+    @answer = Answer.new(set_params)
+    @employee = Employee.find(current_employee.id)
+  end
+
+  def fetch_update_data
+    @answer = Answer.find(params[:id])
+    @employee = Employee.find(current_employee.id)
+  end
+
+  def fetch_password_data
+    @employee_detail = current_employee
+  end
+
+  def save_answer
+    @employee = Employee.find(current_employee.id)
+    @answers = Answer.where(employee_id: current_employee.id)
+    @questions = Question.all
+    @answered = @answers.count
+    @unanswered = @questions.count - @answered
+    flash.now[:success] = 'Answer Saved'
+    refresh
+  end
+
+  def answer_destroy
+    flash.now[:success] = 'Answer deleted successfully.'
+    @employee = Employee.find(current_employee.id)
+    @answers = Answer.where(employee_id: current_employee.id)
+    @questions = Question.all
+    @answered = @answers.count
+    @unanswered = @questions.count - @answered
+  end
+
+  def refresh
+    respond_to do |format|
+      format.js { render 'fresh.js.erb' }
+      format.html
+    end
+  end
+
+  def add
+    respond_to do |format|
+      format.js { render 'add.js.erb' }
+      format.html
+    end
+  end
+
+  def render_edit
+    respond_to do |format|
+      format.js { render 'edit.js.erb' }
+      format.html
+    end
+  end
+
+  def render_password
+    respond_to do |format|
+      format.js { render 'password.js.erb' }
+      format.html
+    end
+  end
 end
+# rubocop:enable Metrics/LineLength
+
+# rubocop:enable Metrics/ClassLength
