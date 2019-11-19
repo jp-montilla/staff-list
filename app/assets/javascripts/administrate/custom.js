8df67c3dbb0fadab28a8239f6696c9c229ea0880
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-11-19 15:02:16 +0800

    js spacing

--- app/assets/javascripts/administrate/custom.js
+++ app/assets/javascripts/administrate/custom.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	$("#serial_number").hide()
-	$("#material_material_type").change(function(){
+  $("#serial_number").hide()
+  $("#material_material_type").change(function(){
     var item=$(this);
     if (item.val() == 'License') {
-  		$("#serial_number").show()
-  	}
-  	else{
-			$("#serial_number").hide()
-  	}
+      $("#serial_number").show()
+    }
+    else{
+      $("#serial_number").hide()
+    }
   });
 });
